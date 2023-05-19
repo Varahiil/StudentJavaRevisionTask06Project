9ffb824d9558052f24974672360e98767ae4187e
Varahiil <Uriilpro33@gmail.com>
2023-05-19 23:34:13 +0300

    Task 03. All Integer Powers of Two [все целые степени двойк]. Well Done.

--- src/by/itstep/javatraining/revision/task/Task03.java
+++ src/by/itstep/javatraining/revision/task/Task03.java
@@ -26,6 +26,14 @@ package by.itstep.javatraining.revision.task;
 
 public class Task03 {
     public static String task03(int number) {
-        return "?";
+        if (number <= 0){
+            return "0";
+        }
+
+        StringBuilder msg = new StringBuilder("1");
+        for (int i = 2; i <= number; i *= 2) {
+            msg.append(" ").append(i);
+        }
+        return msg.toString();
     }
 }
